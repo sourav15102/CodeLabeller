f611212a45888acfa9e31dc744e21f47cde4fcdd
Sourav <souravghai96@gmail.com>
2023-02-03 17:56:58 -0400

    [FEATURE] Spring boot project setup with web-starter, spring-security, postgresql dependencies

---
+++ src/main/java/com/csci5308/codeLabeller/CodeLabellerApplication.java
@@ -0,0 +1,13 @@
+package com.csci5308.codeLabeller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeLabellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeLabellerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csci5308/codeLabeller/CodeLabellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csci5308.codeLabeller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeLabellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
