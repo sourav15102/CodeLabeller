b994ea9d9ec6cd9ee47c35710296d5ef0c05156b
Sourav <souravghai96@gmail.com>
2023-02-20 18:04:09 -0400

    [Modification] little modification where an unknown user wont be able to post a survey to end point

--- SpringBoot-Backend/src/main/java/com/csci5308/codeLabeller/Controller/AdminController.java
+++ SpringBoot-Backend/src/main/java/com/csci5308/codeLabeller/Controller/AdminController.java
@@ -6,8 +6,10 @@ import com.csci5308.codeLabeller.Service.AnnotationService;
 import com.csci5308.codeLabeller.Service.SnippetService;
 import com.csci5308.codeLabeller.Service.SurveyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
@@ -22,11 +24,18 @@ public class AdminController {
     @Autowired
     SnippetService snippetService;
 
+    @Autowired
+    UserDetailsManager jdbcUserDetailsManager;
+
     @PostMapping("{admin_username}/survey/")
-    public void saveSurvey(@PathVariable("admin_username") String username, @RequestBody AdminSnippetsAnnotationsDTO asaDTO){Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    public ResponseEntity<?> saveSurvey(@PathVariable("admin_username") String username, @RequestBody AdminSnippetsAnnotationsDTO asaDTO){Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(!jdbcUserDetailsManager.userExists(username)){
+            return ResponseEntity.notFound().build();
+        }
         asaDTO.setUsername(username);
         CodeSurvey survey = surveyService.createSurvey(asaDTO);
         annotationService.createAnnotations(asaDTO, survey);
         snippetService.createSnippets(asaDTO, survey);
+        return ResponseEntity.ok().build();
     }
 }
