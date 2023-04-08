2a4b1c594c45ac175f04f28b5167c7774dec51aa
= <=>
2023-04-07 23:36:53 -0300

    [DOC] survey approval service doc completed

--- src/main/java/com/csci5308/codeLabeller/Service/SurveyApprovalRequestService.java
+++ src/main/java/com/csci5308/codeLabeller/Service/SurveyApprovalRequestService.java
@@ -2,6 +2,10 @@ package com.csci5308.codeLabeller.Service;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * approving and requesting approval request for survey
+ * will be implemented in future
+ */
 @Service
 public class SurveyApprovalRequestService {
 }
