ae5d3d0cb6b8e9f531e53f706fb2da6126cc2853
= <=>
2023-04-07 22:59:40 -0300

    [TEST] tests modified

--- src/test/java/ServiceTests/StartSurveyServiceTests.java
+++ src/test/java/ServiceTests/StartSurveyServiceTests.java
@@ -4,6 +4,7 @@ import com.csci5308.codeLabeller.Models.CodeSnippet;
 import com.csci5308.codeLabeller.Models.DTO.StartSurveyResponse;
 import com.csci5308.codeLabeller.Service.StartSurveyService;
 import com.csci5308.codeLabeller.Service.SurveyService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,8 +13,12 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @ExtendWith(MockitoExtension.class)
@@ -36,8 +41,17 @@ public class StartSurveyServiceTests {
         long surveyId = 1;
 
         startSurveyService.startTheSurvey(surveyId,page);
+        StartSurveyResponse startSurveyResponse = new StartSurveyResponse();
+        startSurveyResponse.setSnippetId(surveyId);
+        List<StartSurveyResponse> startSurveyResponseList = new ArrayList<>(){{
+            add(startSurveyResponse);
+        }};
+        Page<StartSurveyResponse> startSurveyResponsePage = new PageImpl<>(startSurveyResponseList);
 
-        Mockito.verify(surveyService).startSurvey(surveyId,page);
+        Mockito.lenient().when(surveyService.startSurvey(surveyId,page))
+                .thenReturn(startSurveyResponsePage);
+
+        Assertions.assertTrue(startSurveyResponsePage.getContent().size()==1);
     }
 
 }
